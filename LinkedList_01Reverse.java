d8e71f98a3db5c435a24e23e4243b69cd907d68a
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-06-03 11:29:48 +0530

    remove a specific elment

--- LinkedList_01Reverse.java
+++ LinkedList_01Reverse.java
@@ -4,37 +4,16 @@ import java.util.LinkedList;
 public class LinkedList_01Reverse {
     public static void main(String[] args) {
 
-        // Test 1: Normal list
-        LinkedList<Integer> list1 = new LinkedList<>();
-        Collections.addAll(list1, 1, 2, 3, 4);
-        reverseAndPrint(list1);  // Expected: [4, 3, 2, 1]
+        LinkedList<Integer> l1 = new LinkedList<>();
 
-        // Test 2: Single-node list
-        LinkedList<Integer> list2 = new LinkedList<>();
-        Collections.addAll(list2, 99);
-        reverseAndPrint(list2);  // Expected: [99]
+        Collections.addAll(l1, 1, 2, 3, 2);
 
-        // Test 3: Empty list
-        LinkedList<Integer> list3 = new LinkedList<>();
-        reverseAndPrint(list3);  // Expected: []
-    }
+        Collections.reverse(l1);
 
-    public static void reverseAndPrint(LinkedList<Integer> list) {
-        System.out.println("Original list: " + list);
+        for (int num : l1) {
 
-        int left = 0;
-        int right = list.size() - 1;
+            System.out.println(num);
 
-        while (left < right) {
-            // Swap elements at left and right
-            int temp = list.get(left);
-            list.set(left, list.get(right));
-            list.set(right, temp);
-            left++;
-            right--;
         }
-
-        System.out.println("Reversed list: " + list);
-        System.out.println();
     }
 }
