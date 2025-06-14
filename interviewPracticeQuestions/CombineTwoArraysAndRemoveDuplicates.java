a44d7b186c963c1e1b277de43b8f12559efecc1d
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-06-14 19:53:43 +0530

    practiced questions

---
+++ interviewPracticeQuestions/CombineTwoArraysAndRemoveDuplicates.java
@@ -0,0 +1,25 @@
+package interviewPracticeQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CombineTwoArraysAndRemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] arr1 = {7, -5, 3, 8, -4, 11, -19, 21};
+        int[] arr2 = {6, 13, -7, 0, -4, 3, -5};
+
+        List<Integer> l1 = Stream.concat(Arrays.stream(arr1).boxed(),
+                Arrays.stream(arr2).boxed())
+                .distinct()
+                .collect(Collectors.toList());
+
+        ArrayList<Integer> l2 = new ArrayList<>(l1);
+
+        System.out.println("The combined Arraylist: "+ l2);
+
+    }
+}
